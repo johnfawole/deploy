36c2e7e078c31744cb6a03d227093332d36557a7
baptizer <johnfawole18@gmail.com>
2024-09-17 06:06:42 +0200

    deploy prep

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+ import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract Counter  is ERC20{
     uint256 public number;
+
+ constructor(
+    string memory name,
+    string memory symbol,
+    uint8 decimals,
+    uint256 initialSupply
+ )
+
+ ERC20(name, symbol, decimals) {
+    _mint(msg.sender, initialSupply);
+ }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
